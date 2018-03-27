50a081c302b27641b1c5fb092772e2c9b209e7aa
zhouliang <874329901@qq.com>
2018-03-27 17:30:25 +0800

    引入mint-ui

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import axios from 'axios'
 import {fetch,post} from './utils/axios'
 Vue.use(axios)
+Vue.use(MintUI)
 Vue.config.productionTip = false
 
 Vue.prototype.$fetch=fetch
